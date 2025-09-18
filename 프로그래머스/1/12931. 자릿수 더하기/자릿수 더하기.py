79f1ed9fbabb12b1c694a83a1da228ec53e2e352
Jungil Yang / 양준길 <48828625+wnsrlf0721@users.noreply.github.com>
2025-09-18 18:59:58 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 9.43 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,7 +1,5 @@
 def solution(n):
-    answer = 0
-    for i in str(n):
-        answer += int(i)
+    answer = sum([int(i) for i in str(n)])
     return answer 
 
     # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
