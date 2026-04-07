c82f62f44372ef557ec53026de46933b92fed222
Jungil Yang / 양준길 <48828625+wnsrlf0721@users.noreply.github.com>
2026-04-07 10:32:17 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public double solution(int[] arr) {
+        double answer = 0;
+        for(int idx: arr){
+            answer+=idx;
+        }
+        answer /= arr.length;
+        return answer;
+    }
+}
